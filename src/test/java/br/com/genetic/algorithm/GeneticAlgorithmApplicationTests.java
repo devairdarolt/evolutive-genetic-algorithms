15b9631bbb2073d1aa37c28fb422723c2e4d7b57
Devair D. Darolt <devair.darolt@gesplan.com.br>
2025-08-22 16:36:32 -0300

    Inittial commit

---
+++ src/test/java/br/com/genetic/algorithm/GeneticAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.genetic.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneticAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
